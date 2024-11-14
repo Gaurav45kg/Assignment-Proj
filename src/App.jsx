fae5c22304ea4c8ec241c5424593647f5f7f35a7
Gaurav45kg <gaurav421500@gmail.com>
2024-11-14 18:12:41 +0530

    initial Setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ContactForm } from './pages/ContactForm';
+import { ContactTable } from './pages/ContactTable';
+
+ const App = () => {
+  return (
+    <div>App
+    <ContactForm/>
+    <ContactTable/>
+    </div>
+  )
+}
+export default App;
